fb11bc65737fa58d6a2ffd66bfcaf23567369c45
Zach <Zacaframe@gmail.com>
2020-06-17 20:59:00 -0400

    First push

---
+++ config.py
@@ -0,0 +1,15 @@
+# Census Data API Key
+census_api_key = "d38a2e9fb4b46b223a18e575c7275bbcbfad9482"
+# Census Query Tool https://www.census.gov/data/developers/updates/new-discovery-tool.html
+# API User Guide https://www.census.gov/data/developers/guidance/api-user-guide.Available_Data.html
+
+# County business paterns and non employer statistics 1986-2018
+# https://www.census.gov/data/developers/data-sets/cbp-nonemp-zbp.html
+
+
+
+
+# Bureau of Labor Statistics API Key
+bls_api_key = "f2738a6ce7284d009e12b48734490f6d"
+# BLS Docs https://www.bls.gov/developers/#:~:text=The%20Bureau%20of%20Labor%20Statistics,data%20from%20all%20BLS%20programs.&text=Using%20BLS%20API%20Signatures%2C%20developers,data%2Dinterchange%20or%20XLSX%20format.
+
