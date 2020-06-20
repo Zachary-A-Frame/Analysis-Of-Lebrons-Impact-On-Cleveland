f78eb2966b19fab181df5e0fb797f46f4e53fa14
Zach <Zacaframe@gmail.com>
2020-06-20 09:52:18 -0400

    Added bea api key

--- config.py
+++ config.py
@@ -13,3 +13,5 @@ census_api_key = "d38a2e9fb4b46b223a18e575c7275bbcbfad9482"
 bls_api_key = "f2738a6ce7284d009e12b48734490f6d"
 # BLS Docs https://www.bls.gov/developers/#:~:text=The%20Bureau%20of%20Labor%20Statistics,data%20from%20all%20BLS%20programs.&text=Using%20BLS%20API%20Signatures%2C%20developers,data%2Dinterchange%20or%20XLSX%20format.
 
+
+bea_api_key = "2CE7975D-6464-4C9C-9EE3-E65B0A0C1CB7"
